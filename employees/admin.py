d595986494551f288d25ae224770321174d5e266
Kirill Savenkov <kirill.s@outlook.com>
2023-04-05 01:36:17 +0300

    edit admin site

--- employees/admin.py
+++ employees/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from .models import Department, Employee
 
 
+@admin.register(Department)
 class DepartmentModelAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    list_filter = ['name']
+    ordering = ('name',)
+    list_display = ('name', 'director')
 
 
+@admin.register(Employee)
 class EmployeeModelAdmin(admin.ModelAdmin):
-    list_display = ['last_name']
-    list_filter = ['last_name']
-
-
-admin.site.register(Department)
-admin.site.register(Employee)
+    ordering = ('last_name',)
+    list_display = ('last_name', 'first_name', 'job_title', 'department')
+    list_filter = ('department',)
+    search_fields = ('last_name__startswith',)
